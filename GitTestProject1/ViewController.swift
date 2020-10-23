dc926953db9d54563856b408b5f4ba3ce032c1f3
正木颯 <masakisou@gmail.com>
2020-10-23 09:53:59 +0900

    print("あいうえお"！！！)を追記しました　まさき

--- GitTestProject1/ViewController.swift
+++ GitTestProject1/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("あいうえお！！！")
         // Do any additional setup after loading the view.
     }
 
