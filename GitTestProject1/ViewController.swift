3cb96421171f57d5172947147282f99f2b7a2ce2
正木颯 <masakisou@gmail.com>
2020-10-25 14:13:01 +0900

    print("ABCDE")

--- GitTestProject1/ViewController.swift
+++ GitTestProject1/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         
         print("さしすせそ？？？")
         print("たちつてと")
+        print("ABCDE")
         // Do any additional setup after loading the view.
     }
 
